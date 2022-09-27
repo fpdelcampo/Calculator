86d90092c7fc023537ba8910ae6fe363a963fecc
Francisco del Campo <francisco@Franciscos-Mac-mini.local>
2022-09-27 15:34:29 -0400

    Think it's done for now

--- src/App.js
+++ src/App.js
@@ -2,14 +2,44 @@ import Container from './Components/Container'
 import ResultBar from './Components/ResultBar'
 import Tile from './Components/Tile'
 import TileHolder from './Components/TileHolder'
-import Math from 'mathjs'
 import React, { useState } from "react";
 
-
-const tileValues = ['C', '±', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '^', '='];
+const math = require('mathjs')
+const tileValues = ['C', '±', '%', '/', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '^', '='];
+var init = false
 
 function App() {
+	
 	const [input, setInput] = useState('0');
+	function change(t) {
+		if(t==='C') {
+			init = false
+			return setInput('0')
+		}
+		
+		if(t==='=') {
+			init=false
+			return setInput(math.evaluate(input))
+		}
+		
+		if(t==='±') {
+			if(input[0]==='-') {
+				return setInput(input.substring(1))
+			}
+			else {
+				return setInput('-'+input)
+				}
+			}
+		}
+		
+		if(!init) {
+			init=true
+			return setInput(t)
+		}
+		else {
+			return setInput(input+t)
+		}
+	}
 
 	return (
 		<Container>
@@ -17,14 +47,12 @@ function App() {
 			</ResultBar>
 			<TileHolder tiles= {
 				tileValues.map((tile, i) => {
-					return <Tile key = {i} content={tile} onClick={	
-						() => setInput(input+tile)}></Tile>
+					return <Tile key = {i} content={tile} onClick={() => change(tile, init)}></Tile>
 				})
 			}> 
 			</TileHolder>
 		</Container>
 	);
 }
-
 
 export default App;
